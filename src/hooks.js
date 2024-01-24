7f0fee6e520e8180090cddb45dfdfd55a458d97a
Srini Pandiyan <srinipandiyan7@gmail.com>
2024-01-23 21:53:55 -0600

    added custom hook, useAxios, for PokeDex.js and PlayingCardList.js

--- src/hooks.js
+++ src/hooks.js
@@ -11,4 +11,51 @@ function useFlip(){
     return [isFacingUp, flipCard];
 }
 
-export { useFlip };
+// function useAxios(keyLS, baseURL){
+//     const [resp, setResp] = useLocalStorage(keyLS);
+
+//     const respData = async(formatter = data => data, restOfURL = "") => {
+//         try {
+//         const response = await axios.get(`${baseURL}${restOfURL}`);
+//         setResp(data => [...data, formatter ? formatter(response.data) : response.data]);
+//         } catch (error) {
+//         console.error("Error fetching URL:", error);
+//         }
+//     }
+//     const clearResp = () => setResp([]);
+//     return [resp, respData, clearResp]
+// }
+
+
+function useAxios(keyLS, baseURL) {
+  const [resp, setResp] = useLocalStorage(keyLS);
+
+  const respData = async (restOfURL = "") => {
+    try {
+      const response = await axios.get(`${baseURL}${restOfURL}`);
+      setResp(data => [...data, response.data]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const clearResp = () => setResp([]);
+
+  return [resp, respData, clearResp];
+}
+
+
+function useLocalStorage(key, initialValue = []) {
+  if (localStorage.getItem(key)) {
+    initialValue = JSON.parse(localStorage.getItem(key));
+  }
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+export { useFlip, useAxios, useLocalStorage };
